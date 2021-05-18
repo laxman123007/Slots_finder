88c401f9e8139697be6e5481bd7c59b84d898213
Laxman Raghuwanshi <36527381+laxman123007@users.noreply.github.com>
2021-05-18 10:18:51 +0530

    Cache to False

--- slots_finder.py
+++ slots_finder.py
@@ -8,7 +8,8 @@ from playsound import playsound
 """Run code at hr:00:00 eg 11:00:00, 11:15:00.... etc"""
 # Code Written by Laxman
 monday_date = '17-05-2021'
-pin_codes = ['464001', '464551']
+pin_codes = ['464001', '464551'] # Add more pin codes if you want
+#  '464661'
 
 
 def find_slots():
@@ -16,6 +17,10 @@ def find_slots():
     print(f'Running at time: {datetime.now()}', end=' ')
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
     for pin_code in pin_codes:
+        response_1 = requests.options(
+            f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pin_code}&date={monday_date}',
+            headers=headers)
+        # For cache False
         response = requests.get(
             f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pin_code}&date={monday_date}',
             headers=headers)
@@ -37,7 +42,7 @@ def find_slots():
             print('No Slots Available')
 
 
-schedule.every(10).seconds.do(find_slots)
+schedule.every(5).seconds.do(find_slots)
 
 while True:
     schedule.run_pending()
