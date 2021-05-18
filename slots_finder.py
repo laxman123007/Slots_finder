af98dc694615994a3d5e6f4f601f8702bfd53b77
Laxman Raghuwanshi <36527381+laxman123007@users.noreply.github.com>
2021-05-18 10:28:16 +0530

    Added condition for Dose 1

--- slots_finder.py
+++ slots_finder.py
@@ -8,7 +8,7 @@ from playsound import playsound
 """Run code at hr:00:00 eg 11:00:00, 11:15:00.... etc"""
 # Code Written by Laxman
 monday_date = '17-05-2021'
-pin_codes = ['464001', '464551'] # Add more pin codes if you want
+pin_codes = ['464001', '464551'] # Add more pin codes if you want , 
 #  '464661'
 
 
@@ -17,11 +17,10 @@ def find_slots():
     print(f'Running at time: {datetime.now()}', end=' ')
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
     for pin_code in pin_codes:
-        response_1 = requests.options(
+        response = requests.get(
             f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pin_code}&date={monday_date}',
             headers=headers)
-        # For cache False
-        response = requests.get(
+        response_1 = requests.options(
             f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pin_code}&date={monday_date}',
             headers=headers)
         data = json.loads(response.text)
@@ -30,7 +29,8 @@ def find_slots():
         if len(centers):
             centers_age_18 = [center for center in centers for session in center.get('sessions', []) if
                               session.get('min_age_limit') == 18 and
-                              session.get('available_capacity', 0)]
+                              session.get('available_capacity', 0) and
+                              session.get('available_capacity_dose1', 0)]
             print('Centers Available')
             if len(centers_age_18):
                 print(f'Slots Available for 18+ on Pin Code : {pin_code}')
